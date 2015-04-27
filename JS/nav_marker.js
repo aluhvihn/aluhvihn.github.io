6a76e068d28c08828b3cf409d58b5ea4ac49848f
Alvin Lim <aluhvihn@gmail.com>
2015-04-27 13:05:54 -0700

    test js

---
+++ JS/nav_marker.js
@@ -0,0 +1,33 @@
+var current_id = null;
+
+function highlight(button) {
+	$(button).css("background-color", "#006400");
+	$(button).css("color", "white");
+	return
+}
+
+function current_page()
+	{
+		var url = document.URL;
+		console.log(url);
+		for(i = url.length; i != 0; i--)
+		{
+			if(url[i] == '/')
+				break;
+		}
+		if((i+1) == url.length){
+			highlight(document.getElementById("s-index"));
+			return;
+		}
+		
+		var page = url.substring(i+1, url.length-5);
+		var pull = "s-"+page;
+
+		return pull;
+	}
+
+$(document).ready(function(){
+		current_id = current_page();
+		highlight(document.getElementById(current_id));
+	}
+)
